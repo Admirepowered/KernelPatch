199cddcc7d44d733c5509b2639506e4ca0e1fe39
Admire <admire1971@outlook.com>
2024-04-08 17:14:29 +0800

    umount test

--- kernel/linux/include/linux/fs.h
+++ kernel/linux/include/linux/fs.h
@@ -213,6 +213,11 @@ struct filename
  */
 #define FILE_LOCK_DEFERRED 1
 
+#define MNT_FORCE 0x00000001 /* Attempt to forcibily umount */
+#define MNT_DETACH 0x00000002 /* Just detach from the tree */
+#define MNT_EXPIRE 0x00000004 /* Mark for expiry */
+#define UMOUNT_NOFOLLOW 0x00000008 /* Don't follow symlink on umount */
+#define UMOUNT_UNUSED 0x80000000 /* Flag guaranteed to be unused */
 /* legacy typedef, should eventually be removed */
 typedef void *fl_owner_t;
 struct cred;
@@ -222,6 +227,22 @@ struct path;
 struct dentry;
 struct inode;
 
+struct file_system_type
+{
+    const char *name;
+    int fs_flags;
+};
+
+struct super_block
+{
+    struct list_head s_list; /* Keep this first */
+    dev_t s_dev; /* search index; _not_ kdev_t */
+    unsigned char s_blocksize_bits;
+    unsigned long s_blocksize;
+    loff_t s_maxbytes; /* Max file size */
+    struct file_system_type *s_type;
+    const struct super_operations *s_op;
+};
 extern void kfunc_def(inc_nlink)(struct inode *inode);
 extern void kfunc_def(drop_nlink)(struct inode *inode);
 extern void kfunc_def(clear_nlink)(struct inode *inode);
@@ -245,6 +266,8 @@ extern void kfunc_def(final_putname)(struct filename *name);
 
 extern loff_t kfunc_def(vfs_llseek)(struct file *file, loff_t offset, int whence);
 
+extern int kfunc_def(kern_path)(const char *name, unsigned int flags, struct path *path);
+extern int kfunc_def(path_umount)(struct path *path, int flags);
 //
 
 static inline void inc_nlink(struct inode *inode)
@@ -356,7 +379,15 @@ static inline loff_t vfs_llseek(struct file *file, loff_t offset, int whence)
 {
     kfunc_direct_call(vfs_llseek, file, offset, whence);
 }
+static inline int kern_path(const char *name, unsigned int flags, struct path *path)
+{
+    kfunc_direct_call(kern_path, name, flags, path);
+}
 
+static inline int path_umount(struct path *path, int flags)
+{
+    kfunc_direct_call(path_umount, path, flags);
+}
 static inline void putname(struct filename *name)
 {
     // logkd("aaaaaaaaaaa %llx\n", kfunc(putname));
